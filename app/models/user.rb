9b65f809677b149d2b9987300bd55e8320c51b5f
s05098st <takayuki0405.1186@gmail.com>
2021-02-16 15:27:01 +0900

    バリデーション指摘事項修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :birth_day, presence: true
-
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
-    validates :family_name, presence: true
-    validates :first_name, presence: true
+    validates :nickname
+    validates :family_name
+    validates :first_name
   end
 
   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } do
-    validates :family_name_kana, presence: true
-    validates :first_name_kana, presence: true
+    validates :family_name_kana
+    validates :first_name_kana
   end
+
+  with_options presence: true
+  validates :birth_day
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
