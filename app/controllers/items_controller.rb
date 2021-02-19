be9c8e987a9a130f674cd4e3568f04aca84c6f18
s05098st <takayuki0405.1186@gmail.com>
2021-02-19 18:01:06 +0900

    edit削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,9 +13,6 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def edit
-    @item = Item.find(params[:id])
-  end
 
   def create
     @item = Item.new(item_params)
