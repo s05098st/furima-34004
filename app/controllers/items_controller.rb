59e363372205f3134f10a990515c561fc29f406c
s05098st <takayuki0405.1186@gmail.com>
2021-02-20 11:54:49 +0900

    URLを入力して編集画面に遷移できない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,23 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
-    @items = Item.includes(:user).order("created_at DESC")
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else  
+      render :new
+    end
   end
 
   def show
@@ -18,6 +29,7 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -25,20 +37,20 @@ class ItemsController < ApplicationController
     end
   end
 
-  def create
-    @item = Item.new(item_params)
-    if @item.valid?
-      @item.save
-      redirect_to root_path
-    else  
-      render :new
-    end
-  end
-
   private
 
     def item_params
       params.require(:item).permit(:image, :name, :item_description, :category_id, :item_condition_id, :cost_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
+    end
+
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    def move_to_index
+      if @item.user_id != current_user.id
+        redirect_to root_path
+      end
     end
   end
 
