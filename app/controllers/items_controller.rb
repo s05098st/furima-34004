c2011a08fd8633c2969d9e59d48597f65a7c5eaf
s05098st <takayuki0405.1186@gmail.com>
2021-02-20 12:56:54 +0900

    showとupdateアクション編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,14 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
